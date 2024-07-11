899cfae2e12263375b9bd5243480e085aeb2e056
Erik Movsesyan <erik_movsesyan@epam.com>
2024-07-11 23:41:21 +0400

    Add initial positive and negative unit tests to adhere to TDD flow.

---
+++ StringSum Utility Kata/StringSum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringSum_Utility_Kata
+{
+    public class StringSum
+    {
+        public string Sum(string num1, string num2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StringSum Utility Tests/StringSumTests.cs
@@ -0,0 +1,44 @@
+using FluentAssertions;
+using StringSum_Utility_Kata;
+
+namespace StringSum_Utility_Tests
+{
+    public class StringSumTests
+    {
+        [TestCase("1","2","3")]
+        [TestCase("100","200","300")]
+        [TestCase("4443","2","4445")]
+        public void Sum_WithTwoNaturalParams_ReturnsTheCorrectSummedResult(string num1, string num2, string expectedResult)
+        {
+            //Arrange
+            var utility = new StringSum();
+
+            //Act
+            var actualResult = utility.Sum(num1, num1);
+
+            //Assert
+            actualResult.Should().Be(expectedResult);
+        }
+
+        [TestCase("-1", "2")]
+        [TestCase("2", "-1")]
+        [TestCase("-2", "-2")]
+        [TestCase("0", "0")]
+        [TestCase("0", "1")]
+        [TestCase("1", "0")]
+        [TestCase("test", "3")]
+        [TestCase("3", "3")]
+        [TestCase("test", "test")]
+        public void Sum_WithNonNaturalNumberParameter_Returns0(string num1, string num2)
+        {
+            //Arrange
+            var utility = new StringSum();
+
+            //Act
+            var result = utility.Sum(num1, num2);
+
+            //Assert
+            result.Should().Be("0");
+        }
+    }
+}
